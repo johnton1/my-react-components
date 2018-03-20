13662683748a949e00649699ae260d64639f8ab5
Son Ton <son.ton.tran@gmail.com>
2018-03-19 23:06:59 -0500

    New color scheme

--- src/lib/colorsListConfig.js
+++ src/lib/colorsListConfig.js
@@ -1,7 +1,22 @@
 export const COLORS_SCHEME_LIST = {
 	red: [
-		{ id: 'red1', hex: '#d63031', name: 'dark red' },
-		{ id: 'red3', hex: '#e74c3c', name: 'light red' },
-		{ id: 'red2', hex: '#c0392b', name: 'pomegranate' }
+		{ id: 'red1', hex: '#e74c3c', name: 'light red' },
+		{ id: 'red2', hex: '#d63031', name: 'bright red' },
+		{ id: 'red3', hex: '#c0392b', name: 'pomegranate' }
+	],
+	blue: [
+		{ id: 'blue1', hex: '#74b9ff', name: 'light blue' },
+		{ id: 'blue2', hex: '#00B0FF', name: 'bright blue' },
+		{ id: 'blue3', hex: '#0984e3', name: 'ocean' }
+	],
+	green: [
+		{ id: 'green1', hex: '#55efc4', name: 'light green' },
+		{ id: 'green2', hex: '#00E676', name: 'bright green' },
+		{ id: 'green3', hex: '#00b894', name: 'mint' }
+	],
+	yellow: [
+		{ id: 'yellow1', hex: '#ffeaa7', name: 'light yellow' },
+		{ id: 'yellow2', hex: '#fdcb6e', name: 'bright yellow' },
+		{ id: 'yellow3', hex: '#FFD600', name: 'pumpkin' }
 	]
 };
